97c3bde8e01c82780020050ec0fedf0159966301
iresha92 <ireshahrubasinghe@gmail.com>
2015-09-18 13:39:06 +0530

    google game 4 d final 1

--- core/src/com/example/ucsc/googlegame2/MyGdxGame.java
+++ core/src/com/example/ucsc/googlegame2/MyGdxGame.java
@@ -2,8 +2,11 @@ package com.example.ucsc.googlegame2;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -11,34 +14,31 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 
 	private SpriteBatch batch;
-	private BitmapFont font;
-	private int screenWidth, screenHeight;
-	private String message = "Touch Me";
+	Sound gameOverSound;
+	Music clamJams;
 
 	@Override
 	public void create () {
 		batch=new SpriteBatch();
-
-		screenWidth=Gdx.graphics.getWidth();
-		screenHeight=Gdx.graphics.getHeight();
-
-		font = new BitmapFont();
-		font.setColor(Color.GREEN);
-		//font.scale(5);
-
 		Gdx.input.setInputProcessor(this);
 
+		gameOverSound=Gdx.audio.newSound(Gdx.files.internal("sounds/Death2.wav"));
+		clamJams=Gdx.audio.newMusic(Gdx.files.internal("sounds/clamJam.wav"));
 	}
 
 	//get called at d end of transaction, to avoid memory leaks !!!
 	@Override
 	public void dispose() {
+
+		gameOverSound.dispose();
+		clamJams.dispose();
 		batch.dispose();
-		font.dispose();
 	}
 
 	@Override
@@ -46,34 +46,11 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 		//white color
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		BitmapFont.TextBounds textSize=font.getBounds(message);
-		float x= screenWidth/2=textSize.width/2;
-		float y= screenHeight/2+textSize.height/2;
-
 		batch.begin();
-		font.draw(batch,message,x,y);
 		batch.end();
 	}
 
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		message="Touch down at "+screenX+" , "+screenY;
-		return true;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		message="Touch up at "+screenX+" , "+screenY;
-		return true;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		//message="dragging at "+screenX+" , "+screenY;
-		return false;
-	}
-
+	
 	@Override
 	public boolean keyDown(int keycode) {
 		return false;
@@ -89,6 +66,22 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 		return false;
 	}
 
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		clamJams.play();
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		clamJams.pause();
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
